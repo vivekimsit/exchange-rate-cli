129810e55f45ad394705dc47514853e8dcde77ff
Vivek Poddar <vivekimsit@gmail.com>
2018-05-31 22:05:54 +0100

    Setup.

--- cli.js
+++ cli.js
@@ -1,5 +1,15 @@
 #!/usr/bin/env node
 'use strict';
 
+//const exchange = require('exchange-rate');
 const meow = require('meow');
-const exchange = require('exchange-rate');
+
+const cli = meow(`
+  Usage
+    $ exchange-rate <source> <target>
+  Examples
+    $ cpy EUR GBP
+`);
+
+const [source, target, ..._] = cli.input;
+console.log(source, target);
