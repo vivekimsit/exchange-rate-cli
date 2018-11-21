0c080c67279c1a64283d5e2cc7624bf2fce6a646
Vivek Poddar <vivekimsit@gmail.com>
2018-11-21 16:17:04 +0000

    WIP.

--- cli.js
+++ cli.js
@@ -12,11 +12,39 @@ const cli = meow(`
 `);
 
 async function run() {
-  const source = cli.input[0];
-  const target = cli.input[1];
-  const exchange = createExchange();
-  const {rate} = await exchange.convert({source, target});
-  console.log(rate);
+  if (cli.input.length < 2) {
+    throw new Error('You need to specify both source and target currencies');
+  }
+  const [source, target] = cli.input;
+  try {
+    const exchange = createExchange({
+      requestApi: {
+        async fetch() {
+          return Promise.resolve({
+            body: {
+              'INR': 80 
+            }
+          });
+        }
+      }, 
+      parser: {
+        parse(data) {
+          return { ...data };
+        }
+      },
+      options: {
+        enableCache: false
+      }
+    });
+    const { rate } = await exchange.convert({source, target});
+    console.log(rate);
+  } catch (e) {
+    if (e instanceof createExchange.CurrencyException) {
+      console.error(e.message);
+    } else {
+      throw e;
+    }
+  }
 }
 
 run();
